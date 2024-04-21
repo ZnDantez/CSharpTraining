e8e9c43d52e6c2070c23a6abeb35983cf75233ba
ZnDantez <zn.dantez@gmail.com>
2024-04-21 11:24:27 +0300

    Записал практические задачи второго урока

---
+++ CSharpTrainingP1/Practice02/GreatestCommonDivisor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice02
+{
+    public class GreatestCommonDivisor
+    {
+        //Задача 1. Алгоритм нахождения НОД и организация метода
+        //    Реализовать метод нахождения NOD, используя алгоритм Евклида:
+
+        static int NOD(int a, int b)
+        {
+            while (a != b)
+                if (a > b) a = a - b;
+                else b = b - a;
+            
+            return a;
+        }
+
+        public static void Do()
+        {
+            int a = 532;
+            int b = 224;
+            Console.WriteLine(NOD(a, b));
+        }
+
+    }
+}
---
+++ CSharpTrainingP1/Practice02/SumOfLastDigits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice02
+{
+    public class SumOfLastDigits
+    {
+        //Задача 2. Сумма двух последних цифр
+        //    Вывести в диапазоне от 10 до 100 все числа,
+        //    сумма двух последних цифр которых равна 10:
+
+        static bool Check(int a)
+        {
+            if ((a % 10 + a / 10 % 10 == 10)) return true;
+            else return false;
+        }
+
+        public static void Do()
+        {
+            for (int i = 10; i <= 100; i++)
+                if (Check(i)) Console.WriteLine(i);
+        }
+
+    }
+}
