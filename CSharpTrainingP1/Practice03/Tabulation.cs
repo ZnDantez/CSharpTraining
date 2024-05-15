e8f020517fe0fa18992add7f76bf5cabb1225dd0
ZnDantez <zn.dantez@gmail.com>
2024-05-15 12:50:06 +0300

    Кажется, что-то не влезло в прошлый коммит

---
+++ CSharpTrainingP1/Practice03/Tabulation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice03
+{
+    public class Tabulation
+    {
+
+        //        Задача 3. Написать программу табуляции произвольной функции в диапазоне
+        //от a до b.
+        //Написать программу табуляции произвольной функции в диапазоне от a до b.Функция задаётся
+        //программно.
+
+        #region a
+
+        public static void Do1()
+        {
+            //а) Решение без использования ООП.
+
+            double a = -5;
+            double b = 5;
+            double h = 0.5;
+            Console.WriteLine("{0,10}{1,10}", "x", "F(x)");
+            for (double x = a; x <= b; x = x + h)
+            {
+                Console.WriteLine("{0,10}{1,10:f3}", x, F(x));
+            }
+
+
+        }
+
+        static double F(double x)
+        {
+            return 1 / x;
+        }
+
+        #endregion
+
+        public static void Do2()
+        {
+            //б) Решение с использованием ООП.
+
+        }
+
+
+    }
+}
